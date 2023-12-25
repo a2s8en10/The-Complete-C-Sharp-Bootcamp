8b02dfc4ea8754faef6a7d7fa00ecba5892b0d37
Anurag Sahu <anu.sahu9589@gmail.com>
2023-12-25 14:38:49 +0530

    update

---
+++ FirstConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FirstConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
