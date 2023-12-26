98a864a0ec89efa774c0bda7107c13c55421280a
Anurag Sahu <anu.sahu9589@gmail.com>
2023-12-26 19:04:46 +0530

    update

---
+++ ForLoop condition/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForLoop_condition
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the number : ");
+            int a = int.Parse(Console.ReadLine());
+
+            for(int i = 1; i <= a; i++)
+            {
+                int sum;
+                sum = i * i * i ;
+
+                Console.WriteLine("Number is : {0} and cube of the {1} is :{2}", i,i,sum);
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
