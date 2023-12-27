4d2aeea2e503d28fe40899d908895b0198f039b2
Anurag Sahu <anu.sahu9589@gmail.com>
2023-12-28 00:08:40 +0530

    update

---
+++ prime numbers/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace prime_numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 13;
+            for (int i = 0; i < 13; i++)
+            {
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
