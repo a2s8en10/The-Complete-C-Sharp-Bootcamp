14d6f562c459ca1abf86b15eec4af8cf3bf1b893
Anurag Sahu <anu.sahu9589@gmail.com>
2023-12-28 17:39:59 +0530

    update

--- prime numbers/Program.cs
+++ prime numbers/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Runtime.ConstrainedExecution;
 
 namespace prime_numbers
 {
@@ -6,12 +7,48 @@ namespace prime_numbers
     {
         static void Main(string[] args)
         {
-            int a = 13;
-            for (int i = 0; i < 13; i++)
-            {
-                Console.WriteLine();
-            }
+            //Console.WriteLine("Enter the starting number : ");
+            //int a = int.Parse(Console.ReadLine());
 
+            //Console.WriteLine("Enter the Ending number : ");
+            //int b = int.Parse(Console.ReadLine());
+
+            //for (int i = a; i <= b; i++)
+            //{
+            //    int c = 0;
+
+            //    for (int j = 2; j <= i / 2; j++)
+            //    {
+            //        if (i % j == 0)
+            //        {
+            //            c++;
+            //            break;
+            //        }
+            //    }
+
+            //    if (c == 0 && i != 1)
+
+            //        Console.Write("{0} ", i);
+            //}
+
+            //Console.WriteLine("Check the nmber is prime or not prime....");
+            //Console.WriteLine("Enter the number");
+            //int e = int.Parse(Console.ReadLine());
+
+            //for (int i = 2; i <= i - 1; i++)
+            //{
+            //    if (e % i == 0)
+            //    {
+            //        Console.WriteLine("Its a not prime :");
+            //    }
+            //    else
+            //    {
+            //    Console.WriteLine("Its a prime :");
+            //    }
+
+
+            
+            Console.ReadLine();
         }
     }
 }
