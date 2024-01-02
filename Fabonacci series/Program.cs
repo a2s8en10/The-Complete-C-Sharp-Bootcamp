8a579b5a912db94d722b1ce3dd1dc14339c2ac0a
Anurag Sahu <anu.sahu9589@gmail.com>
2024-01-02 12:40:38 +0530

    update

---
+++ Fabonacci series/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Fabonacci_series
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the number : ");
+            int n = int.Parse(Console.ReadLine());
+
+            int num = 0, sum = 0;
+            // int num = 0;
+            int temp = 1;
+            Console.Write("Fabonacci series is :"+num);
+            for (int i = 0; i < n; i++)
+            {
+                sum = num + temp;
+                num = temp;
+                temp = sum;
+
+                Console.Write(" {0}", num);
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Fabonacci/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Fabonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
