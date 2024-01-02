41b15a32c3831d421dc0d816fcde3a85d7b16a4b
Anurag Sahu <anu.sahu9589@gmail.com>
2024-01-02 13:15:12 +0530

    update

--- Palindrome With For Loop/Program.cs
+++ Palindrome With For Loop/Program.cs
@@ -13,16 +13,13 @@ namespace Palindrome_With_For_Loop
             Console.Write("Enter the number : ");
             int temp = int.Parse(Console.ReadLine());
 
-            int sum, num, g,t, count = 1, i = 0;
+            int sum, num, g, i = 0;
             g = temp;
-            t = temp;
-            for (int j = 1; j <=count; j++)
+            string value = temp.ToString();
+            int b = value.Length;
+
+            for (int j = 1; j <= b; j++)
             {
-                while (t > 0)
-                {
-                    count++;
-                    t = t / 10;
-                }
                 num = temp % 10;
                 sum = temp / 10;
                 temp = sum;
