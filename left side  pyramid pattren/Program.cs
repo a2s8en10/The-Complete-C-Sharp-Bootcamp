c1214e224e42847e82d00d3eab4db6f9fc0d6e69
Anurag Sahu <anu.sahu9589@gmail.com>
2024-01-03 19:14:55 +0530

    update

---
+++ left side  pyramid pattren/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace left_side__pyramid_pattren
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+             Console.WriteLine("Enter the number : ");
+            int a = int.Parse(Console.ReadLine());
+
+            int spac = 1;
+
+            for (int i = 1; i <= a; i++)
+            {
+                for (int k = spac; k <= a; k++)
+                {
+                    Console.Write(" ");
+                }
+                for (int j = i; j <= a; j++)
+                {
+                    Console.Write("* ");
+                }
+                Console.WriteLine();
+                spac--;
+            }
+            Console.ReadLine();
+        }
+    }
+}
