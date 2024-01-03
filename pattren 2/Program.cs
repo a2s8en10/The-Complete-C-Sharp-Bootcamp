53360dc523994234ed5d2123c123143d77d01af2
Anurag Sahu <anu.sahu9589@gmail.com>
2024-01-03 21:02:53 +0530

    update

---
+++ pattren 2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace pattren_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            Console.WriteLine("Enter the number :");
+            n = int .Parse(Console.ReadLine());
+
+            int i = 2;//row
+            while (i <= n)
+
+            {
+                int j = 1; // col
+                while (j <= i)
+
+                {
+                    Console.Write("*");
+                    j = j + 1;
+                }
+
+                Console.WriteLine();
+                i = i + 2;
+            }
+        }
+    }
+}
