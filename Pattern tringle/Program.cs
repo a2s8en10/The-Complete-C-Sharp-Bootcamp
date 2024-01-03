7e835c6b7ddaebef63531fa1f6abe5901fb1649c
Anurag Sahu <anu.sahu9589@gmail.com>
2024-01-03 18:30:31 +0530

    update

--- Pattern tringle/Program.cs
+++ Pattern tringle/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 
-namespace pyramid_pattern
+namespace Pattern_tringle
 {
     internal class Program
     {
@@ -8,6 +8,7 @@ namespace pyramid_pattern
         {
             Console.Write("Enter the number : ");
             int a = int.Parse(Console.ReadLine());
+
             int spc;
             spc = a - 1;
             for (int i = 1; i <= a; i++)
@@ -19,7 +20,7 @@ namespace pyramid_pattern
 
                 for (int j = 1; j <= i; j++)
                 {
-                    Console.Write(" *");
+                    Console.Write("*");
                 }
 
                 Console.WriteLine();
