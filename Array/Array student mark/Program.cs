3a442b9c7312e9224a8446cb5d4ba9eca34ec5c7
Anurag Sahu <anu.sahu9589@gmail.com>
2024-01-07 16:28:53 +0530

    update

---
+++ Array/Array student mark/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Array_student_mark
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] student = new int[5];
+            student[0] = 34;
+            student[1] = 56;
+            student[2] = 78;
+            student[3] = 65;
+            student[4] = 87;
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Marks of student : "+student[i]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
