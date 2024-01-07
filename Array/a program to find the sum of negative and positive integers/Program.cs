8d8289d046e82b7a6fd06aa6c3c37e26c427fc76
Anurag Sahu <anu.sahu9589@gmail.com>
2024-01-07 18:45:39 +0530

    update

---
+++ Array/a program to find the sum of negative and positive integers/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace a_program_to_find_the_sum_of_negative_and_positive_integers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Array/a program to input a string through the keyboard and print the same/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace a_program_to_input_a_string_through_the_keyboard_and_print_the_same
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the name :");
+            string str = Console.ReadLine();
+
+            string[] name = new string[5];
+            for (int i = 0; i < 5; i++)
+            {
+                string a = Console.ReadLine();
+                name[i] = a;
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
