8ad8b72c7680257b09a7d906d70f3ece748b0ebe
Anurag Sahu <anu.sahu9589@gmail.com>
2024-01-07 18:30:42 +0530

    update

---
+++ Array/a program to print a string using an array/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace a_program_to_print_a_string_using_an_array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Enter the number");
+            //int num = int.Parse(Console.ReadLine());
+
+
+            int num = 5;
+            string[] str = new string[num];
+            str[0] = "Anurag";
+            str[1] = "Pride";
+            str[2] = "Anura sahu";
+            str[3] = "Anu";
+            str[4] = "sahu";
+
+            Console.WriteLine("Enter the index number ");
+            int i = int.Parse(Console.ReadLine());
+            Console.WriteLine(str[i]);
+
+
+            //Console.WriteLine("Enter the {0} name :", num);
+            //for (int i = 0; i < num; i++)
+            //{
+            //    string s = Console.ReadLine();
+            //    str[i] = s;
+            //}
+
+            //foreach (string name in str)
+            //{
+            //    Console.WriteLine(name);
+            //}
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Array/a program to print the average marks obtained by a student in five tests/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace a_program_to_print_the_average_marks_obtained_by_a_student_in_five_tests
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number of subject : ");
+            int num = int.Parse(Console.ReadLine());
+            int sum = 0;
+            int[] average = new int[num];
+
+            Console.WriteLine("Enter the marks : ");
+            for (int i = 0; i < num; i++)
+            {
+                int a = int.Parse(Console.ReadLine());
+                average[i] = a;
+            }
+
+            for (int i = 0; i < average.Length; i++)
+            {
+                sum = sum + average[i];
+            }
+
+            Console.WriteLine("total number : " + sum);
+            sum = sum / num;
+            Console.WriteLine("the_average_marks_obtained_by_a_student : " + sum);
+            Console.ReadLine();
+        }
+    }
+}
