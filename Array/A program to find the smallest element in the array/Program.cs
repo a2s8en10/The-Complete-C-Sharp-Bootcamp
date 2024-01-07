1fc00c335e7e2a439a6d2b71b924d860667443ed
Anurag Sahu <anu.sahu9589@gmail.com>
2024-01-07 19:57:52 +0530

    update

--- Array/A program to find the smallest element in the array/Program.cs
+++ Array/A program to find the smallest element in the array/Program.cs
@@ -6,7 +6,17 @@ namespace A_program_to_find_the_smallest_element_in_the_array
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Enter the length of array : ");
+            int a = int.Parse(Console.ReadLine());
+            int[] num = new int[a];
+
+            for (int i = 0; i < a; i++)
+            {
+                int b = int.Parse(Console.ReadLine());
+                num[i] = b;
+            }
+            Array.Sort(num);
+            Console.WriteLine("the smallest element in the array : {0}", num[0]);
         }
     }
 }
