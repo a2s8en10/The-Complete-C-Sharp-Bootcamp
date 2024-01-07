b92e3c4dc39ec3d4a3c5be63f3d15ad0f733dc6c
Anurag Sahu <anu.sahu9589@gmail.com>
2024-01-07 19:08:57 +0530

    update

--- Array/a program to find the sum of negative and positive integers/Program.cs
+++ Array/a program to find the sum of negative and positive integers/Program.cs
@@ -6,7 +6,34 @@ namespace a_program_to_find_the_sum_of_negative_and_positive_integers
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int sum = 0;
+            Console.Write("Enter the length of Array : ");
+            int a = int .Parse(Console.ReadLine());
+
+            int[] num = new int[a];
+            Console.WriteLine("Enter the number ");
+
+            for (int i = 0; i < num.Length; i++)
+            {
+                int n = int.Parse(Console.ReadLine());
+                num[i] = n;
+            }
+
+            foreach (int item in num)
+            {
+                sum = sum + item;
+            }
+
+            if (sum > 0)
+            {
+                Console.WriteLine("positive integers : {0}", sum);
+            }
+            else
+            {
+                Console.WriteLine("negetive integers : {0}", sum);
+            }
+
+            Console.ReadLine();
         }
     }
 }
