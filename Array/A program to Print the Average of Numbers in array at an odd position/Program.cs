7d1b98d1f0d1e72fe4d27c141586c7bf016347bd
Anurag Sahu <anu.sahu9589@gmail.com>
2024-01-08 12:45:26 +0530

    update

--- Array/A program to Print the Average of Numbers in array at an odd position/Program.cs
+++ Array/A program to Print the Average of Numbers in array at an odd position/Program.cs
@@ -6,7 +6,28 @@ namespace A_program_to_Print_the_Average_of_Numbers_in_array_at_an_odd_position
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Enter the length of Array :");
+            int a = int.Parse(Console.ReadLine());
+            int[] num = new int[a];
+
+            for (int i = 0; i < a; i++)
+            {
+                int b = int.Parse(Console.ReadLine());
+                num[i] = b;
+            }
+
+            int j = 1;
+            double sum = 0;
+            int count = 0;
+            while (j <= (num.Length - 1))
+            {
+                sum = sum + num[j];
+                j = j + 2;
+                count++;
+            }
+            sum = sum / count;
+            Console.WriteLine("The Average of Numbers in array at an odd position : {0}", sum);
+            Console.ReadLine();
         }
     }
 }
---
+++ Array/A program to print array elements in reverse order/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace A_program_to_print_array_elements_in_reverse_order_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the length of Array :");
+            int a = int.Parse(Console.ReadLine());
+
+            int[] num = new int[a];
+            Console.WriteLine("Enter the number :");
+            for (int i = 0; i < num.Length; i++)
+            {
+                int b = int.Parse(Console.ReadLine());
+                num[i] = b;
+            }
+
+            Console.WriteLine("Array elements in reverse order ....");
+            for (int i = num.Length - 1; i >= 0; i--)
+            {
+                Console.WriteLine(num[i]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
