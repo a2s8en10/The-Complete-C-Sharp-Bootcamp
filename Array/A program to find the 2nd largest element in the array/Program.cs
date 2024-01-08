dd3a44ea9d08f979054ee035fe937ea8ac2f45f6
Anurag Sahu <anu.sahu9589@gmail.com>
2024-01-08 11:22:10 +0530

    update

---
+++ Array/A program to find the 2nd largest element in the array/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace A_program_to_find_the_2nd_largest_element_in_the_array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the length of Array :");
+            int a = int .Parse(Console.ReadLine());
+            int[] num = new int[a];
+
+            Console.WriteLine("Enter the number :");
+            for (int i = 0; i < num.Length; i++)
+            {
+                int b = int.Parse(Console.ReadLine());
+                num[i] = b;
+            }
+            Array.Sort(num);
+            Console.WriteLine("The 2nd largest element in the array : {0}", num[a-2]);
+            Console.ReadLine();
+        }
+    }
+}
--- Array/A program to find the 2nd smallest element in the array/Program.cs
+++ Array/A program to find the 2nd smallest element in the array/Program.cs
@@ -6,7 +6,17 @@ namespace A_program_to_find_the_2nd_smallest_element_in_the_array
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Enter the length of Array : ");
+            int c = int.Parse(Console.ReadLine());
+            int[] num = new int[c];
+
+            for (int i = 0; i < num.Length; i++)
+            {
+                int b = int.Parse(Console.ReadLine());
+                num[i] = b;
+            }
+            Array.Sort(num);
+            Console.WriteLine("The 2nd smallest element in the array : {0}", num[1]);
         }
     }
 }
