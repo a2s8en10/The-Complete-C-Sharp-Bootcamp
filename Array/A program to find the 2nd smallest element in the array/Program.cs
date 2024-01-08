bb67edeb90eb784269b1d65a4c7a4f2e9dc27e29
Anurag Sahu <anu.sahu9589@gmail.com>
2024-01-08 11:09:52 +0530

    update

---
+++ Array/A program to find the 2nd smallest element in the array/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace A_program_to_find_the_2nd_smallest_element_in_the_array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Array/A program to find the largest element in the array/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace A_program_to_find_the_largest_element_in_the_array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the length of Arrray : ");
+            int a = int.Parse(Console.ReadLine());
+            int[] num = new int[a];
+
+            Console.WriteLine("Enter the number : ");
+            for (int i = 0; i < a; i++)
+            {
+                int b = int.Parse(Console.ReadLine());
+                num[i] = b;
+            }
+            Array.Sort(num);
+            Console.WriteLine("The largest element in the array : {0}", num[a-1]);
+            Console.ReadLine();
+        }
+    }
+}
