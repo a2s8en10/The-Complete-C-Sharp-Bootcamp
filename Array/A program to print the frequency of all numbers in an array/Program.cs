4179c52c64b991b5e022c839e09c5e553789514d
Anurag Sahu <anu.sahu9589@gmail.com>
2024-01-09 18:24:04 +0530

    Add code

---
+++ Array/A program to print the frequency of all numbers in an array/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace A_program_to_print_the_frequency_of_all_numbers_in_an_array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Array/A program to separate even and odd numbers in an array/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace A_program_to_separate_even_and_odd_numbers_in_an_array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the length of Array : ");
+            int a = int.Parse(Console.ReadLine());
+            int[] num = new int[a];
+
+            Console.WriteLine("Enter the number:");
+            for (int i = 0; i < a; i++)
+            {
+                int b = int.Parse(Console.ReadLine());
+                num[i] = b;
+            }
+
+            for (int i = 0; i < num.Length; i++)
+            {
+
+                if (num[i] % 2 == 0)
+                {
+                    Console.WriteLine("Even number in an Array : {0}", num[i]);
+                }
+                else
+                {
+                    Console.WriteLine("Odd number in an Array : {0}", num[i]);
+                }
+            }
+
+
+        }
+    }
+}
