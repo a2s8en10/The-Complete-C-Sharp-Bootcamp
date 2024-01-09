a495a0edfcc9c43355eeea2a9e165b6d5fb2f038
Anurag Sahu <anu.sahu9589@gmail.com>
2024-01-09 20:24:54 +0530

    Add code

--- Array/A program to print the frequency of all numbers in an array/Program.cs
+++ Array/A program to print the frequency of all numbers in an array/Program.cs
@@ -6,7 +6,33 @@ namespace A_program_to_print_the_frequency_of_all_numbers_in_an_array
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Enter the length of Array : ");
+            int a = int.Parse(Console.ReadLine());
+            int[] num = new int[a];
+
+            Console.WriteLine("Enter the number:");
+            for (int i = 0; i < a; i++)
+            {
+                int b = int.Parse(Console.ReadLine());
+                num[i] = b;
+            }
+            Array.Sort(num);
+            int externalCount=0;
+            for (int i = externalCount; i < num.Length; i++)
+            {
+                int count = 1;
+                
+                for (int j = i+1; j < num.Length; j++)
+                {
+                    if (num[externalCount] == num[j])
+                    {
+                        count++;
+                    }
+                }
+                externalCount=count;
+                Console.WriteLine("The frequency of all numbers in an array {0} = {1}", num[i], count);
+
+            }
         }
     }
 }
