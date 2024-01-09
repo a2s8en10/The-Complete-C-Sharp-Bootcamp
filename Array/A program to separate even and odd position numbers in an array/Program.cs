5c9f74a7665f7b5140f3dda3ec82f58d62bebd42
Anurag Sahu <anu.sahu9589@gmail.com>
2024-01-09 20:47:13 +0530

    Add code

---
+++ Array/A program to separate even and odd position numbers in an array/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace A_program_to_separate_even_and_odd_position_numbers_in_an_array_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the length of Array : ");
+            int a = int.Parse(Console.ReadLine());
+            int[] num = new int[a];
+
+            Console.WriteLine("Enter the number :");
+            for (int i = 0; i < num.Length; i++)
+            {
+                int b = int.Parse(Console.ReadLine());
+                num[i] = b;
+            }
+
+            int count = 0;
+            for (int i = 0; i < num.Length; i++)
+            {
+                if (i == count)
+                {
+                    Console.WriteLine("Even position numbers in an array : {0}", num[i]);
+                    count = count + 2;
+                }
+                else
+                {
+                    Console.WriteLine("Odd position numbers in an array : {0}", num[i]);
+                }
+            }
+
+
+
+        }
+    }
+}
