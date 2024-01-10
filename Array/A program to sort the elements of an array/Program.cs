2ce6fae93a17f2849371677db182eafce158700a
Anurag Sahu <anu.sahu9589@gmail.com>
2024-01-10 16:14:30 +0530

    Add code

---
+++ Array/A program to sort the elements of an array/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace A_program_to_sort_the_elements_of_an_array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the lenfth of Array :");
+            int a = int.Parse(Console.ReadLine());
+            int[] num = new int[a];
+
+            Console.WriteLine("Enter the number :");
+            for (int i = 0; i < num.Length; i++)
+            {
+                int b = int.Parse(Console.ReadLine());
+                num[i] = b;
+            }
+            Array.Sort(num);
+            Console.WriteLine("sort the elements of an array.....");
+
+            foreach (int i in num)
+            {
+                Console.WriteLine(i);
+            }
+
+
+
+
+        }
+    }
+}
