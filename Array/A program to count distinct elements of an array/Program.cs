5476e07ee7f049d076e14f6197c6d31c935e00c9
Anurag Sahu <anu.sahu9589@gmail.com>
2024-01-11 17:11:30 +0530

    Add code

--- Array/A program to count distinct elements of an array/Program.cs
+++ Array/A program to count distinct elements of an array/Program.cs
@@ -18,33 +18,20 @@ namespace A_program_to_count_distinct_elements_of_an_array
                 num[i] = b;
             }
 
-            //int count = 1;
-            //for (int i = 0; i < num.Length; i++)
-            //{
-            //    for (int j = 0; j < num.Length; j++)
-            //    {
-            //        if (num[i] != num[j])
-            //        {
-            //            count++;
-            //        }
-            //    }
-            //}
-            //Console.WriteLine("count distinct elements of an array : {0}",count);
-
-            int res = 1;
-
-            // Pick all elements one by one
-            for (int i = 1; i < num.Length; i++)
+            int count = 1;
+            for (int i = 0; i < num.Length; i++)
             {
-                int j = 0;
-                for (j = 0; j < num.Length; j++)
-                    if (num[i] == num[j])
-                        break;
+                for (int j = 0; j < num.Length; j++)
+                {
+                    if (num[i] != num[j])
+                    {
+                        count++;
+                    }
 
-                // If not printed earlier, then print it
-                if (i == j)
-                    res++;
+                }
             }
+            Console.WriteLine("count distinct elements of an array : {0}", count);
+
         }
     }
 }
---
+++ Array/A program to remove duplicate elements in an array/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Drawing;
+
+namespace A_program_to_remove_duplicate_elements_in_an_array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the length of Array : ");
+            int a = int.Parse(Console.ReadLine());
+
+            int[] num = new int[a];
+
+            Console.WriteLine("Enter the element : ");
+            for (int i = 0; i < num.Length; i++)
+            {
+                int b = int.Parse(Console.ReadLine());
+                num[i] = b;
+            }
+            Array.Sort(num);
+            int j = 0;
+            for (int i = 0; i < num.Length - 1; i++)
+            {
+                if (num[i] != num[i + 1])
+                {
+                    num[j++] = num[i];
+                }
+            }
+            num[j++] = num[num.Length - 1];
+
+            Console.WriteLine("After removing duplicate element from array:");
+
+            for (int i = 0; i < j; i++)
+                 Console.Write(" " + num[i]);
+
+
+        }
+    }
+}
