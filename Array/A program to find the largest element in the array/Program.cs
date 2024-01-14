67bebeb203a4f8f993ed9d5ff0effa089c3a5fe3
Anurag Sahu <anu.sahu9589@gmail.com>
2024-01-14 23:42:31 +0530

    Add code

--- Array/A program to find the largest element in the array/Program.cs
+++ Array/A program to find the largest element in the array/Program.cs
@@ -7,17 +7,32 @@ namespace A_program_to_find_the_largest_element_in_the_array
         static void Main(string[] args)
         {
             Console.Write("Enter the length of Arrray : ");
-            int a = int.Parse(Console.ReadLine());
-            int[] num = new int[a];
+            int c = int.Parse(Console.ReadLine());
+            int[] num = new int[c];
 
             Console.WriteLine("Enter the number : ");
-            for (int i = 0; i < a; i++)
+            for (int i = 0; i < c; i++)
             {
                 int b = int.Parse(Console.ReadLine());
                 num[i] = b;
             }
-            Array.Sort(num);
-            Console.WriteLine("The largest element in the array : {0}", num[a-1]);
+            int a = 0;
+            for (int i = 0; i < num.Length; i++)
+            {
+                for (int j = 0; j < num.Length - 1; j++)
+                {
+                    if (num[j] > num[j + 1])
+                    {
+                        a = num[j + 1];
+
+                        num[j + 1] = num[j];
+
+                        num[j] = a;
+                    }
+                }
+
+            }
+            Console.WriteLine("The largest element in the array : {0}", num[num.Length - 1]);
             Console.ReadLine();
         }
     }
