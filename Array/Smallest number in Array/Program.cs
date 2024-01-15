e896be4498ab2a2e71c31f8fe67a2a96baf4889e
Anurag Sahu <anu.sahu9589@gmail.com>
2024-01-15 13:18:04 +0530

    Add code

---
+++ Array/Smallest number in Array/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Smallest_number_in_Array
+{
+    class Func
+    {
+        public
+            int[] num = new int[5] { 1, 2, 3, 34, 73 };
+
+        public void num1()
+        {
+            // Array length 
+            int count = 0;
+            foreach (int i in num)
+            {
+                count++;
+            }
+
+            // sort Array
+            int a = 0;
+            for (int i = 0; i < count; i++)
+            {
+                for (int j = 0; j < count - 1; j++)
+                {
+                    if (num[j] > num[j + 1])
+                    {
+                        a = num[j + 1];
+
+                        num[j + 1] = num[j];
+
+                        num[j] = a;
+                    }
+                }
+
+            }
+
+            Console.Write("Smallest value in Array : " + num[0]);
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Func func = new Func();
+            func.num1();
+            Console.ReadLine();
+        }
+    }
+}
