71b227799c693b4e69d9bcfce4e7fb4da08ad089
Anurag Sahu <anu.sahu9589@gmail.com>
2024-01-15 11:59:13 +0530

    Add code

---
+++ Array/Function sum of all element of array/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Function_sum_of_all_element_of_array
+{
+    class Func
+    {
+        public
+            int[] num1 = new int[5] { 2, 5, 3, 8, 4 };
+        int sum = 0;
+
+        public void Add()
+        {
+            foreach (int i in num1)
+            {
+                sum = sum + i;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Func func = new Func();
+            func.Add();
+            Console.WriteLine();
+        }
+    }
+}
