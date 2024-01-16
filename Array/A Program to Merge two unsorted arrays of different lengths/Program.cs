8b5060ae1ba725107e11413f3993e36af51d58cc
Anurag Sahu <anu.sahu9589@gmail.com>
2024-01-16 12:09:32 +0530

    Add code

---
+++ Array/A Program to Merge two unsorted arrays of different lengths/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace A_Program_to_Merge_two_unsorted_arrays_of_different_lengths
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the length of array 1st :");
+            int a = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter the length of array 2nd :");
+            int b = int.Parse(Console.ReadLine());
+
+            int[] num1 = new int[a];
+            int[] num2 = new int[b];
+
+            Console.WriteLine("Enter the element 1st array :");
+            for (int i = 0; i < a; i++)
+            {
+                int c = int.Parse(Console.ReadLine());
+                num1[i] = c;
+            }
+
+            Console.WriteLine("Enter the element 2nd array :");
+            for (int i = 0; i < b; i++)
+            {
+                int d = int.Parse(Console.ReadLine());
+                num1[i] = d;
+            }
+
+            int length1 = 0;
+            int length2 = 0;
+
+            foreach (int i in num1)
+            {
+                length1++;
+            }
+
+            foreach (int i in num2)
+            {
+                length2++;
+            }
+
+            int length = 0;
+            length = length1 + length2;
+
+            int[] num = new int[length];
+
+            //for (int i = 0; i < length; i++)
+            //{
+            //    Console.WriteLine(num[i]);
+            //}
+
+            //Console.WriteLine("Merge two unsorted arrays");
+            //foreach (int i in num1)
+            //{
+            //    Console.WriteLine(i);
+            //}
+            //foreach(int i in num2)
+            //{
+            //    Console.WriteLine(i);
+            //}
+        }
+    }
+}
---
+++ Array/A Program to Sort an array in Descending order/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace A_Program_to_Sort_an_array_in_Descending_order_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the length of Array :");
+            int a = int.Parse(Console.ReadLine());
+
+            int[] arr = new int[a];
+            Console.WriteLine("Enter the Element :");
+
+            for (int i = 0; i < a; i++)
+            {
+                int b = int.Parse(Console.ReadLine());
+                arr[i] = b;
+            }
+            // Find Array length 
+            int count = 0;
+            foreach (int item in arr)
+            {
+                count++;
+            }
+
+            // Find sort Array 
+            int temp;
+            for (int i = 0; i < count; i++)
+            {
+                for (int j = 0; j < count- 1; j++)
+                {
+                    if (arr[j] > arr[j + 1])
+                    {
+                        temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
+                    }
+                }
+            }
+
+            // Descending order loop 
+            Console.WriteLine("Sort an array in Descending order ");
+            for (int i = count -1 ;i >=0; i--)
+            {
+                Console.WriteLine(arr[i]);
+            }
+
+        }
+    }
+}
