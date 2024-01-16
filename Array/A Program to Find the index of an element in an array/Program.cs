1c676c45c245614379f53962a365581cdd42b267
Anurag Sahu <anu.sahu9589@gmail.com>
2024-01-16 11:15:12 +0530

    Add code

---
+++ Array/A Program to Find the index of an element in an array/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace A_Program_to_Find_the_index_of_an_element_in_an_array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
