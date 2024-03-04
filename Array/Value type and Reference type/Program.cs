ac231e188ae9f3e858cfc98a9004b66e0ff833e1
Anurag Sahu <anu.sahu9589@gmail.com>
2024-03-04 22:53:09 +0530

    Add code

---
+++ Array/Value type and Reference type/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Value_type_and_Reference_type
+{
+
+    //struct Employee 
+    //{
+    //    public int salary;
+    //    public int age;
+    //}
+    class Employee
+    {
+        public int salary;
+        public int age;
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee e = new Employee();
+            e.salary = 1000;
+            e.age = 23;
+
+            Employee e1 = e;
+            Employee e2 = e1;
+
+            e.age = 20;
+
+            Console.WriteLine(e.age);
+            Console.WriteLine(e1.age);
+            Console.WriteLine(e2.age);
+
+            Console.ReadLine();
+        }
+    }
+}
