d9e96d7269517c6fca2901a2fe86e175b52c5766
Anurag Sahu <anu.sahu9589@gmail.com>
2024-03-08 15:05:11 +0530

    Add code
    Static class

---
+++ Array/Constructor/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Constructor
+{
+    class user
+    {
+        public int id;
+        public string name;
+        public int age;
+        public string email;
+        //public user() // default constructor  
+        //{
+        //    Console.WriteLine("constructor are called");
+        //}
+        public user (int id,string name, int age , string email )
+        {
+            this.id = id;
+            this.name = name;
+            this.age = age;
+            this.email = email;
+        }
+
+    }
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            user con = new user();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Array/Static_classs/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Static_classs
+{
+    static class school
+    {
+        public static string name;
+        public static int stander;
+        public static int fees;
+
+        static school ()
+        {
+            name = "Taksh Shila Academy";
+            stander = 12;
+            fees = 18000;
+        }
+
+        public static void getschooldetail ()
+        {
+            Console.WriteLine("School name is : {0}" ,name);
+            Console.WriteLine("School in Class Room is : {0}", stander);
+            Console.WriteLine("School Fees Amount is : {0}", fees);
+        }
+
+
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            school.getschooldetail();
+            Console.ReadLine();
+        }
+    }
+}
