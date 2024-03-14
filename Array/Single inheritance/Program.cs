d1f886843b2c6e2728a605068d61951503632f88
Anurag Sahu <anu.sahu9589@gmail.com>
2024-03-14 22:28:08 +0530

    Add code
    Single Inheritance

---
+++ Array/Single inheritance/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Single_inheritance
+{
+    class baseclass
+    {
+        public void show1()
+        {
+            Console.WriteLine("this is base class....");
+        }
+    }
+
+    class driveclass : baseclass // this is the single inheritance 
+    {
+        public void show2()
+        {
+            Console.WriteLine("this is drive class....");
+        }
+    }
+    internal class Programs
+    {
+        static void Main(string[] args)
+        {
+            driveclass sub = new driveclass();
+            sub.show1();
+            sub.show2();
+            Console.ReadLine();s
+        }
+    }
+}
