3408bda3d4d7e37599a68dd2662cda89552013bf
Anurag Sahu <anu.sahu9589@gmail.com>
2024-03-15 23:37:50 +0530

    Add code
    Constructor in Inheritance

---
+++ Array/Constructor Inheritance/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Constructor_Inheritance
+{
+    class baseclass
+    {
+        public baseclass(string name) 
+        {
+            Console.WriteLine(name);
+        }
+    }
+    class deriveclass : baseclass
+    {
+        public deriveclass() : base ("Anurag")
+        {
+            Console.WriteLine("This is deriveclass contructor...");
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            deriveclass anu = new deriveclass();
+            Console.ReadLine();
+        }
+    }
+}
