48d9d3ab433b5bac43c7958ae4d283751c345600
Anurag Sahu <anu.sahu9589@gmail.com>
2024-03-16 23:21:30 +0530

    Add code
    PRIVATE_ACCESS_SPECIFIERS

---
+++ Array/PRIVATE_ACCESS SPECIFIERS/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace PRIVATE_ACCESS_SPECIFIERS
+{
+    public class subclass
+    {
+        private void show1()
+        {
+            Console.WriteLine("Method is private");
+        }
+        public void show2()
+        {
+            subclass sub1 = new subclass();
+            sub1.show1();
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //subclass sub = new subclass   // create private objact 
+            //sub.show1();
+
+            subclass sub2 = new subclass();
+            sub2.show2();
+            Console.ReadLine();
+        }
+    }
+}
