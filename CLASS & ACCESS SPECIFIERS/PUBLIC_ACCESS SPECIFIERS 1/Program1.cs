4b9ea2fb605627ef291d753caa387dee005b3d43
Anurag Sahu <anu.sahu9589@gmail.com>
2024-03-16 21:12:07 +0530

    Add code

---
+++ CLASS & ACCESS SPECIFIERS/PUBLIC_ACCESS SPECIFIERS 1/Program1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ACCESS_SPECIFIERS_1
+{
+    public class info
+    {
+        public void show1()
+        {
+            Console.WriteLine("Anurag");
+        }
+        //public void show2()
+        //{
+        //    info x = new info();
+        //    x.show1();
+        //}
+    }
+    internal class Program1 //: info
+    {
+        static void Main(string[] args)
+        {
+            //info anu = new info();
+            //anu.show2();
+
+            //Program1 anu = new Program1 ();
+            //anu.show1();
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ CLASS & ACCESS SPECIFIERS/PUBLIC_ACCESS SPECIFIERS 2/Program2.cs
@@ -0,0 +1,20 @@
+﻿using ACCESS_SPECIFIERS_1;
+using System;
+
+namespace ACCESS_SPECIFIERS_2
+{
+    internal class Program2 : info 
+    {
+        static void Main(string[] args)
+        {
+            //info sub = new info();
+            //sub.show1();
+
+            Program2 sub1 = new Program2();
+            sub1.show1();
+
+
+            Console.ReadLine();
+        }
+    }
+}
