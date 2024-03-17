02e269baf5729324f87f1e9f14098526f882a383
Anurag Sahu <anu.sahu9589@gmail.com>
2024-03-17 10:58:56 +0530

    Add code
    PROTECTED_ACCESS SPECIFIERS

---
+++ Array/PROTECTED_ACCESS SPECIFIERS 2/Program2.cs
@@ -0,0 +1,15 @@
+﻿using PROTECTED_ACCESS_SPECIFIERS;
+using System;
+
+namespace PROTECTED_ACCESS_SPECIFIERS_2
+{
+    internal class Program2 : class1
+    {
+        static void Main(string[] args)
+        {
+            Program2 p2 = new Program2();
+            p2.show1();
+            Console.ReadLine();
+        }
+    }
+}
