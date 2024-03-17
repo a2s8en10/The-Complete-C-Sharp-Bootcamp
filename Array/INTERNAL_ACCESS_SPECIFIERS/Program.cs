71d4fe3979a6fce0e6bffa1de9a7b51ca5486a20
Anurag Sahu <anu.sahu9589@gmail.com>
2024-03-17 10:22:01 +0530

    Add code
    INTERNAL_ACCESS_SPECIFIERS

---
+++ Array/INTERNAL_ACCESS_SPECIFIERS/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace INTERNAL_ACCESS_SPECIFIERS
+{
+    public class class1
+    {
+        internal void show1()
+        {
+            Console.WriteLine("this is internal");
+        }
+        internal void show2()
+        {
+            class1 c1 = new class1();
+            c1.show1();
+        }
+
+    }
+
+    internal class Program : class1
+    {
+        static void Main(string[] args)
+        {
+            class1 c2 = new class1();
+            c2.show2();
+
+            class1 c3 = new class1();
+            c3.show1();
+
+            Program p1 = new Program();
+            p1.show1();
+
+            Console.ReadLine();
+        }
+    }
+}
