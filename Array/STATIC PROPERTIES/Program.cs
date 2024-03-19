b48287eb432518abfb6903be5cb15027c3ee36a4
Anurag Sahu <anu.sahu9589@gmail.com>
2024-03-19 20:46:32 +0530

    Add code
    STATIC PROPERTIES

---
+++ Array/STATIC PROPERTIES/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace STATIC_PROPERTIES
+{
+    class student
+    {
+        private static string name;
+        private static string id;
+
+        public static string _name
+        {
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    Console.WriteLine("Its not null and empty");
+                }
+                else
+                {
+                    name = value;
+                }
+            }
+            get
+            {
+                return name;
+            }
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            student._name = null;
+            Console.WriteLine(student._name);
+            Console.ReadLine();
+        }
+    }
+}
