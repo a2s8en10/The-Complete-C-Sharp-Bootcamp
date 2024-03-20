4d8a55fab5e53930431d23fb232cad43a9ea02f8
Anurag Sahu <anu.sahu9589@gmail.com>
2024-03-20 23:10:48 +0530

    Add code
    METHOD OVERLOADING

---
+++ Polymorphishm/METHOD OVERLOADING/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace METHOD_OVERLOADING
+{
+    internal class Program
+    {
+        public void add ()
+        {
+            int a = 12;
+            int b = 13;
+            int c = a+b;
+            Console.WriteLine(c);
+        }
+        public void add(int a,int b)
+        {
+           int c = a+b;
+            Console.WriteLine(c);
+        }
+        public void add(string a,string b)
+        {
+            string c = a + " " +b;
+            Console.WriteLine(c);
+        }
+        public void add(float a, float b)
+        {
+           float c = a + b;
+            Console.WriteLine(c);
+        }
+        static void Main(string[] args)
+        {
+            Program p1 = new Program();
+            p1.add();
+            p1.add(2,3);
+            p1.add("anurag","sahu");
+            p1.add(12.2f, 123.4f);
+
+
+           Console.ReadLine();
+        }
+    }
+}
