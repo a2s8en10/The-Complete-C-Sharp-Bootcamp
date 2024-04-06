d0517d9795aa49826938961cb062ca0992a02951
Anurag Sahu <anu.sahu9589@gmail.com>
2024-04-06 08:30:23 +0530

    Add code
    METHOD OVERRIDING (RUN TIME POLYMORPHISM)

---
+++ Polymorphishm/METHOD OVERRIDING/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace METHOD_OVERRIDING
+{
+    class parant // parant class use the VIRTUAL keyword with METHOD_OVERRIDING
+    {
+        public virtual void show ()
+        {
+            Console.WriteLine("parant class method !");
+        }
+    }
+    class child : parant    // child class use the OVERRIDING keyword with METHOD_OVERRIDING
+    {
+        public override void show ()
+        {
+            Console.WriteLine("child class method !");  // CALL PARANT CLASS { base.show() }
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            child p = new child ();   // USE ANY METHOD CALL PARANT CLASS WITH OBJECT 
+            ((parant)p).show ();      // RESULT ONLY CHILD CLASS BECAUSE METHOD OVERRIDING
+            Console.ReadLine ();
+        }
+    }
+}
