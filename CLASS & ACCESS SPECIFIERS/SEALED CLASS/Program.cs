22d476a64a1d6967a044c17f25a8082d2096151b
Anurag Sahu <anu.sahu9589@gmail.com>
2024-04-07 18:40:31 +0530

    Add code
    SEALED CLASS

---
+++ CLASS & ACCESS SPECIFIERS/SEALED CLASS/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace SEALED_CLASS
+{
+    sealed class parant  // sealed class are not inherit
+    {
+        public void show()
+        {
+            Console.WriteLine("parant class invoke !");
+        }
+    }
+    //class child : parant
+    //{
+    //    public void show1()
+    //    {
+    //        Console.WriteLine("Child class invoke !");
+    //    }
+    //}
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //child c = new child();
+            //c.show();
+            
+            parant p = new parant();
+            p.show();
+            Console.ReadLine();
+        }
+    }
+}
