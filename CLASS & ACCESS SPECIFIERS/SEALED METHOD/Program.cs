f27d70081f3ba359d6cc23cc5c40c8b02ec9feab
Anurag Sahu <anu.sahu9589@gmail.com>
2024-04-08 23:10:32 +0530

    Add code
    SEALED METHOD

---
+++ CLASS & ACCESS SPECIFIERS/SEALED METHOD/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace SEALED_METHOD
+{
+    // sealed method is use because cannot create a child class and override classes
+    class A
+    {
+        public virtual void show ()
+        {
+            Console.WriteLine("base class...");
+        }
+    }
+    class B : A
+    {
+        public sealed override void show()
+        {
+            Console.WriteLine("Derive class...");
+        }
+    }
+    //class c : B   // sealed class cannot create a child class
+    //{
+    //    public override void show()
+    //    {
+    //        Console.WriteLine("Derive class...");
+    //    }
+    //}
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            B obj = new B();
+            obj.show();
+            Console.ReadLine();
+        }
+    }
+}
