69a98d47bd169c491f31c0320d0671a5512ba213
Anurag Sahu <anu.sahu9589@gmail.com>
2024-04-11 19:05:47 +0530

    Add code
    SINGLE CAST DELEGATES

---
+++ CLASS & ACCESS SPECIFIERS/SINGLE CAST DELEGATES/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace SINGLE_CAST_DELEGATES
+{
+    public delegate void calculation(int num1,  int num2);
+    internal class Program
+    {
+        public static void Add(int a ,int b)
+        {
+            int c = a + b;
+            Console.WriteLine("Addition is {0}",c);
+        }
+        static void Main(string[] args)
+        {
+            calculation obj = new calculation(Add);
+            obj.Invoke(10, 20);
+            Console.ReadLine();
+        }
+    }
+}
