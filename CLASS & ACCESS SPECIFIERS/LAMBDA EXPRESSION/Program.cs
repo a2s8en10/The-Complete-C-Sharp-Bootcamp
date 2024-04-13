28d6147448471c72eea5601a66083ecf52e7a876
Anurag Sahu <anu.sahu9589@gmail.com>
2024-04-14 00:30:19 +0530

    Add code
    LAMBDA EXPRESSION

---
+++ CLASS & ACCESS SPECIFIERS/LAMBDA EXPRESSION/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace LAMBDA_EXPRESSION
+{
+    public delegate void lambda(int num);
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            lambda obj1 = (b) => b++;
+            obj1.Invoke(2);
+
+
+            //lambda obj = (a) =>
+            //{
+            //    a += 10;
+            //    Console.WriteLine(a);
+            //};
+            //// obj(20);
+            //obj.Invoke(5);
+
+            Console.ReadLine();
+        }
+    }
+}
