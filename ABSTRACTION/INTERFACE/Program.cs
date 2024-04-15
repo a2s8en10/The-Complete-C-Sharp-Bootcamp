7763a1c3df48ee1a33951933cf62cdb24b0fedaf
Anurag Sahu <anu.sahu9589@gmail.com>
2024-04-15 20:16:07 +0530

    Add code
    INTERFACE

---
+++ ABSTRACTION/INTERFACE/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace INTERFACE
+{
+    interface person // define only properties , method , and events which is the member of the interface
+                     // interface contain only the declaration of the member.
+    {
+        void show ();
+    }
+    class student : person // it is the responsibility of the deriving class to define the member
+    {
+        public void show()
+        {
+            Console.WriteLine("This is called interface ! ");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            student anu = new student();
+            anu.show();
+            Console.ReadLine();
+        }
+    }
+}
