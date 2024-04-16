3c5daa4ebd78d17423f4b0e61525df192f214f54
Anurag Sahu <anu.sahu9589@gmail.com>
2024-04-16 20:11:48 +0530

    Add code

---
+++ ABSTRACTION/EXPLICIT IMPLEMENTATION OF INTERFACE/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace EXPLICIT_IMPLEMENTATION_OF_INTERFACE
+{
+    interface a1
+    {
+        void show();
+    }
+    interface a2
+    {
+        void show();
+    }
+    class Myinterface :  a1,a2 
+        {
+        public void show()
+        {
+            Console.WriteLine("this is the interface of A1 !!");
+        }
+
+        //void a1.show()
+        //{
+        //    Console.WriteLine("this is the interface of A1 !!");
+        //}
+
+        void a2.show()
+        {
+            Console.WriteLine("this is the interface of A2 !!");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            a1 obj1 = new Myinterface();
+            obj1.show();
+
+            a2 obj2 = new Myinterface();
+            obj2.show();
+            //Myinterface obj = new Myinterface();
+            //obj.show();
+            //((a2)obj).show();
+
+            //Myinterface obj = new Myinterface();
+            //((a1)obj).show();
+            //((a2)obj).show();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ABSTRACTION/INTERFACE INHERITANCE/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace INTERFACE_INHERITANCE
+{
+    interface a1
+    {
+        void show1();
+    }
+    interface a2
+    {
+        void show2();
+    }
+    interface a3 : a1,a2
+    {
+        void show3();
+    }
+    internal class Program : a3
+    {
+
+        public void show1()
+        {
+            Console.WriteLine("the interface of A1");
+        }
+
+        public void show2()
+        {
+            Console.WriteLine("the interface of A2");
+        }
+
+        public void show3()
+        {
+            Console.WriteLine("the interface of A3");
+        }
+        static void Main(string[] args)
+        {
+           
+        }
+    }
+}
