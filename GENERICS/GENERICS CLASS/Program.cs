66776e777cc4cf8b67bb9ff0b9967b9e1170d326
Anurag Sahu <anu.sahu9589@gmail.com>
2024-04-17 21:05:57 +0530

    Add code
    GENERICS CLASS

---
+++ GENERICS/GENERICS CLASS/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace GENERICS_CLASS
+{
+    class generic<T>
+    {
+        private T num;
+
+        //public T show 
+        //{
+        //    set
+        //    {
+        //        this.num = value;
+        //    }
+        //    get
+        //    {
+        //        return this.num;
+        //    }
+
+        //}
+        public generic(T a) 
+        {
+            this.num = a;
+        }
+        public T get()
+        {
+            return this.num;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            generic<int> g = new generic<int>(2);
+            Console.WriteLine(g.get());
+
+            generic<string> g1 = new generic<string>("Anurag sahu");
+            Console.WriteLine(g1.get());
+
+            generic<char> g2 = new generic<char>('R');
+            Console.WriteLine(g2.get());
+
+            Console.ReadLine();
+        }
+    }
+}
