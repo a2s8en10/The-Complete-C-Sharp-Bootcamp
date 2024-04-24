9814201a803ab6675d9dbc59b86ab716357b6a66
Anurag Sahu <anu.sahu9589@gmail.com>
2024-04-24 21:07:08 +0530

    Add code
    LIST

--- GENERICS COLLECTION/LIST/Program.cs
+++ GENERICS COLLECTION/LIST/Program.cs
@@ -8,35 +8,35 @@ namespace LIST
     {
         static void Main(string[] args)
         {
-            //Employee emp1 = new Employee()
-            //{
-            //    name = "Anurag",
-            //    id = 1,
-            //    age = 20
-            //};
-            //Employee emp2 = new Employee()
-            //{
-            //    name = "pride",
-            //    id = 2,
-            //    age = 21
-            //};
-            //Employee emp3 = new Employee()
-            //{
-            //    name = "Relax",
-            //    id = 3,
-            //    age = 22
-            //};
+            Employee emp1 = new Employee()
+            {
+                name = "Anurag",
+                id = 1,
+                age = 20
+            };
+            Employee emp2 = new Employee()
+            {
+                name = "pride",
+                id = 2,
+                age = 18
+            };
+            Employee emp3 = new Employee()
+            {
+                name = "Relax",
+                id = 3,
+                age = 42
+            };
 
 
-            //List<Employee> myemployee = new List<Employee>();
-            //myemployee.Add(emp1);
-            //myemployee.Add(emp2);
-            //myemployee.Add(emp3);
+            List<Employee> myemployee = new List<Employee>();
+            myemployee.Add(emp1);
+            myemployee.Add(emp2);
+            myemployee.Add(emp3);
 
-            //foreach (Employee emp in myemployee)
-            //{
-            //    Console.WriteLine("name : {0}  id : {1}  age : {2}",emp.name,emp.id,emp.age);
-            //}
+            foreach (Employee emp in myemployee)
+            {
+                Console.WriteLine("name : {0}  id : {1}  age : {2}", emp.name, emp.id, emp.age);
+            }
 
             //List<char> mychar = new List<char>();
             //mychar.Add('A');
@@ -47,19 +47,43 @@ namespace LIST
             //List<string> mystring = new List<string>();
             //mystring.Add("Anurag");
             //mystring.Add("Relax");
+            //mystring.Add("Sahu");
             //mystring.Add("Pride");
             //mystring.Add("Sahu");
 
-            List<int> mynumbers = new List<int>();
-            mynumbers.Add(33);
-            mynumbers.Add(11);
-            mynumbers.Add(44);
-            mynumbers.Add(22);
-            foreach (int item in mynumbers)
-            {
-                Console.WriteLine(item);
-            }
-            //Console.WriteLine("--------------------------");
+            //foreach (string item in mystring)
+            //{
+            //    Console.WriteLine(item);
+            //}
+            Console.WriteLine("--------------------------");
+           //Employee emp = myemployee.Find(myemployee => myemployee.age > 20);
+           // foreach (Employee e in myemployee)
+           // {
+           //     Console.WriteLine("name : {0}  id : {1}  age : {2}", e.name, e.id, e.age);
+           // }
+            //mystring.Clear();
+
+            //Console.WriteLine(mystring.LastIndexOf("Sahu")); //show the index number last value
+            //Console.WriteLine(mystring.IndexOf("Sahu"));  // show the index number first value
+
+            //mystring.Reverse();
+
+            //Console.WriteLine(mystring.Exists(mystring => mystring.StartsWith("A")));  // return the boolean value
+            //Console.WriteLine(mystring.Exists(mystring => mystring.EndsWith("u")));  // return the boolean value
+
+            //Console.WriteLine(mystring.Contains("Anurag"));  // return boolean value
+            //foreach (string item in mystring)
+            //{
+            //    Console.WriteLine(item);
+            //}
+            //List<int> mynumbers = new List<int>();
+            //mynumbers.Add(33);
+            //mynumbers.Add(11);
+            //mynumbers.Add(44);
+            //mynumbers.Add(22);
+
+            // ---------------  PROPERTIES  --------------
+
             //mynumbers.Sort();
             //mynumbers.InsertRange(2, mynumbers); // insert index (index,object name)
             //mynumbers.Insert(2,66);  // value insert (index,value)
