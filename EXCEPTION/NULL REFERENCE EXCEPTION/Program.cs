1219c3752f1fd5c6acd471460a967047a6069ebc
Anurag Sahu <anu.sahu9589@gmail.com>
2024-04-27 23:08:27 +0530

    Add code
    NULL REFERENCE EXCEPTION

---
+++ EXCEPTION/NULL REFERENCE EXCEPTION/Program.cs
@@ -0,0 +1,23 @@
+﻿
+using System;
+
+namespace NULL_REFERENCE_EXCEPTION
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string name = null;
+                Console.WriteLine(name);
+            }
+            catch (NullReferenceException ex)
+            {
+                Console.WriteLine("string is null");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
