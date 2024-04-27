02d703f814cc3b770908bc55b3649f62ff52afe4
Anurag Sahu <anu.sahu9589@gmail.com>
2024-04-27 23:14:24 +0530

    Add code
    FORMATE EXCEPTION

---
+++ EXCEPTION/FORMATE EXCEPTION/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace FORMATE_EXCEPTION
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter the number : ");
+            string number = Console.ReadLine();
+
+            try
+            {
+                int num = int.Parse(number);
+                Console.WriteLine(num);
+            }
+            catch ( FormatException ex )
+            {
+                Console.WriteLine("string formate is invailid...");
+            }
+            Console.ReadLine();
+        }
+    }
+}
