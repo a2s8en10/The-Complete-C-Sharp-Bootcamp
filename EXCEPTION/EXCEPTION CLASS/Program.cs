2680903a4c89a5fe23ad7169f4806da1374a1d99
Anurag Sahu <anu.sahu9589@gmail.com>
2024-04-27 23:42:15 +0530

    Add code
    EXCEPTION CLASS

---
+++ EXCEPTION/EXCEPTION CLASS/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace EXCEPTION_CLASS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int a = 10;
+                int b = 0;
+                int c = a / b;
+                Console.WriteLine(c);
+
+                //int[] a = new int[3];
+                //a[0] = 1;
+                //a[1] = 2;
+                //a[2] = 3;
+                //a[3] = 4;
+                //foreach (int i in a)
+                //{
+                //    Console.WriteLine(i);
+                //}
+
+                //string a = null;
+                //Console.WriteLine(a.Length);
+
+                //string a = "fhd";
+                //int num = int.Parse(a);
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.ReadLine();
+        }
+    }
+}
