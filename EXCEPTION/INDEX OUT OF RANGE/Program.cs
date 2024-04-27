73c089a8fc1e3c6ab50fd2508ac4ae7cca9d433a
Anurag Sahu <anu.sahu9589@gmail.com>
2024-04-27 22:49:36 +0530

    Add code
    INDEX OUT OF RANGE

---
+++ EXCEPTION/INDEX OUT OF RANGE/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace INDEX_OUT_OF_RANGE
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[3];
+            try
+            {
+                arr[0] = 1;
+                arr[1] = 2;
+                arr[2] = 3;
+                arr[3] = 4;
+
+                foreach (int i in arr)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            catch (IndexOutOfRangeException Ex)
+            {
+                Console.WriteLine("Array`s length is out of range...");
+                Console.WriteLine(Ex.Message);
+            }
+            Console.ReadLine();
+        }
+    }
+}
