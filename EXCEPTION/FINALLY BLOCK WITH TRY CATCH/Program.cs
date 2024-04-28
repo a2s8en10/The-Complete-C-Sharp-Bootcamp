262a1ae21735b08451cc77346271be7f043a47fc
Anurag Sahu <anu.sahu9589@gmail.com>
2024-04-28 23:25:44 +0530

    Add code
    FINALLY BLOCK WITH TRY CATCH

---
+++ EXCEPTION/FINALLY BLOCK WITH TRY CATCH/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace FINALLY_BLOCK_WITH_TRY_CATCH
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string a = null;
+                Console.WriteLine(a.Length);
+            }
+            catch (NullReferenceException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally         // finally block are always execute
+            {
+                Console.WriteLine("finally block are execute...");
+            }
+            Console.ReadLine();
+        }
+    }
+}
