2991424b2258d2097223aea44d8de5c7f17691f1
Anurag Sahu <anu.sahu9589@gmail.com>
2024-04-28 23:03:30 +0530

    Add code
    TRY WITH MULTIPLE CATCH BLOCKS

---
+++ EXCEPTION/TRY WITH MULTIPLE CATCH BLOCKS/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace TRY_WITH_MULTIPLE_CATCH_BLOCKS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int a = 12;
+                int b = 0;
+                int c = a / b;
+
+                string name = null;
+                Console.WriteLine(name.Length);
+
+                int[] num = new int[3];
+                num[0] = 1;
+                num[1] = 2;
+                num[2] = 3;
+                num[3] = 4;
+
+            }
+            catch (DivideByZeroException ex )
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch(NullReferenceException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.ReadLine();
+        }
+    }
+}
