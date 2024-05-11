d465e89ef19d1862f162780df53652bfba4e888a
Anurag Sahu <anu.sahu9589@gmail.com>
2024-05-11 23:44:01 +0530

    Add code
    IS or AS

---
+++ IS or AS/IS or AS/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IS_or_AS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 1;
+            //bool b = a is int;
+            //Console.WriteLine(b);
+
+            if(a is int)
+            {
+                Console.WriteLine("A is int");
+            }
+            else
+            {
+                Console.WriteLine("A is another type");
+            }
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
