71447efba91fbb9438b9233749f495fb3d9df246
Anurag Sahu <anu.sahu9589@gmail.com>
2024-05-11 23:01:31 +0530

    Add code
    GUID

---
+++ GUID/Guid/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Globally_Unique_Identifier
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Guid a = new Guid();
+            //Console.WriteLine(a.ToString().Replace("-",string.Empty)); // remove (-) 
+            //Console.WriteLine(a.ToString("N")); // remove (-)
+            Console.WriteLine(a.ToString().Substring(0,15)); // Identifier digit 
+
+            Console.ReadLine();
+        }
+    }
+}
