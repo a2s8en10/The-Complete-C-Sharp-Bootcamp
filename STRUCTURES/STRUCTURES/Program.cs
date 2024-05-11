141b94f2caecb93d4e7290ee00ee8ca617a2a283
Anurag Sahu <anu.sahu9589@gmail.com>
2024-05-11 11:50:10 +0530

    Add code
    STRUCTURE

---
+++ STRUCTURES/STRUCTURES/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace STRUCTURES
+{
+    struct Program
+    {
+        public void func()
+        {
+            Console.WriteLine("function call first's.....");
+        }
+        static void Main(string[] args)
+        {
+            //Program p = new Program(); // structure and class object create
+            //or 
+            Program p; // without constructor structure object create
+            p.func();
+            Console.ReadLine();
+        }
+    }
+}
