41e60a3d99dc6f3c1e26907d694d6d00639896ec
Anurag Sahu <anu.sahu9589@gmail.com>
2024-05-12 13:05:01 +0530

    Add code
    Null Coalesce Operator

---
+++ Null Coalesce/Null Coalesce Operator/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Null_Coalesce_Operator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = null;
+            string result = name ?? "Enter the string..."; // it is null coalesh operator
+            Console.WriteLine(result);
+
+            string f1 = null;
+            string f2 = null;
+            string f3 = null;
+
+            string result1 = f1 ?? f2 ?? f3 ?? "fruit name...";   // first is call f1 if f1 is null then f2 f2 is null then f3 
+            Console.WriteLine(result1);
+
+            //if (name != null)
+            //{
+            //    result = name;
+            //}
+            Console.ReadLine();
+
+        }
+    }
+}
