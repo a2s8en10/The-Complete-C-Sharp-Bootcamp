585d9196de2658aaf1a018f66d8da7dfff945661
Anurag Sahu <anu.sahu9589@gmail.com>
2024-06-18 22:20:30 +0530

    Add code
    Count duplicate elements in an array

---
+++ C# Sharp Array  Exercises, Practice/Count duplicate elements in an array/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Count_duplicate_elements_in_an_array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Array Length : ");
+            int n = int.Parse(Console.ReadLine());
+            int[] num = new int[n];
+
+            Console.Write("Array Element : ");
+            for (int i = 0; i < n; i++)
+            {
+                int a = int.Parse(Console.ReadLine());
+                num[i] = a;
+            }
+            int count = 0;
+            for (int i = 0;i < n; i++)
+            {
+                for (int j = i+1; j < n; j++)
+                {
+                    if (num[i] == num[j])
+                    {
+                        count++;
+                    }
+                }
+            }
+            Console.WriteLine("Total number of duplicate elements found in the array is :{0}",count);
+
+            Console.ReadLine();
+        }
+    }
+}
