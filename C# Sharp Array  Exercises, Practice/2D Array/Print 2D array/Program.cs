41e00157349b71d050b29b95a9ed64053e1db97d
Anurag Sahu <anu.sahu9589@gmail.com>
2024-07-17 20:35:24 +0530

    Add Code
    PRINT 2D ARRAY

---
+++ C# Sharp Array  Exercises, Practice/2D Array/Print 2D array/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Print_2D_array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Console.Write("Enter the Order of the Matrix : ");
+            int m = int.Parse(Console.ReadLine());
+            int n = int.Parse(Console.ReadLine());
+            Console.Clear();
+            Console.Write("Enter the Order of the Matrix : [{0} , {1}]",m,n);
+            int[,] A = new int[10, 10];
+            Console.Write("\nEnter The Matrix Elements : ");
+                for (int i = 0; i < m; i++)
+                {
+                    for (int j = 0; j < n; j++)
+                    {
+                        A[i, j] = int.Parse(Console.ReadLine());
+                    }
+                }
+
+            Console.WriteLine("\nMatrix : ");
+                for (int i = 0; i < m; i++)
+                {
+                    for (int j = 0; j < n; j++)
+                    {
+                        Console.Write(A[i, j] + " ");
+
+                    }
+                    Console.WriteLine();
+                }
+
+                // Console.Clear();
+                //Console.WriteLine("\nMatrix A : ");
+                //for (int i = 0; i < m; i++)
+                //{
+                //    for (int j = 0; j < n; j++)
+                //    {
+                //        Console.Write(A[i, j] + " ");
+
+                //    }
+                //    Console.WriteLine();
+                //}
+                //Console.WriteLine("Transpose Matrix : ");
+
+                //for (int i  = 0; i < m; i++)
+                //{
+                //    for (int j = 0; j < n; j++)
+                //    {
+                //        Console.Write(A[j, i] + " ");
+
+                //    }
+                //    Console.WriteLine();
+                //}
+                Console.Read();
+        }
+    }
+}
---
+++ C# Sharp Array  Exercises, Practice/2D Array/Print 2D array/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Print 2D array")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Print 2D array")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("45426621-fbf3-426f-af39-3901454a50ca")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
